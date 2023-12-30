e00b0cd6d3ce2563f5dc57023caf684c9d9a149d
LarsThoennes <larsthoennes@gmail.com>
2023-12-30 22:28:21 +0100

    add background-class.js / compress function draw() in world.class.js

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,10 @@
 class Chicken extends MovableObject {
-    
+    y = 370;
+    height = 70;
+    width = 70;
     constructor(){
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.x = 100 + Math.random() * 500;
+        
     }
 }
