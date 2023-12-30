903a5e69a4aed3db07e0d77b4c3798d8b87beffd
LarsThoennes <larsthoennes@gmail.com>
2023-12-30 23:21:37 +0100

    add function animate() in character.class.js / chicken.class.js

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,9 +2,24 @@ class Chicken extends MovableObject {
     y = 370;
     height = 70;
     width = 70;
+    IMAGES_WALKING = [
+        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
+    ];
     constructor(){
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.x = 100 + Math.random() * 500;
-        
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
+    }
+    animate(){
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+            this.x -= 1;
+        },200);
     }
 }
