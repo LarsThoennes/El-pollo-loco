4c1181fc3178023f126935af9873575521c4275b
LarsThoennes <larsthoennes@gmail.com>
2023-12-31 14:28:06 +0100

    Added level.class.js and level1.js

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,7 +1,7 @@
 class Cloud extends MovableObject {
     y = 50;
     width = 500;
-    height = 250;
+    height = 250; 
     constructor(){
         super().loadImage('img/5_background/layers/4_clouds/1.png');
         this.x = Math.random() * 500;
