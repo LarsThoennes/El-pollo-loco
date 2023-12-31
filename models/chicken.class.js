e4ace6ae72fa4a4a0d93cd6d23870ce2669b5bc0
LarsThoennes <larsthoennes@gmail.com>
2023-12-31 12:15:24 +0100

    character made movable on the x axis

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -7,19 +7,22 @@ class Chicken extends MovableObject {
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
     ];
+    
     constructor(){
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
-        this.x = 100 + Math.random() * 500;
+        this.x = 200 + Math.random() * 500;
+        this.speed = 0.15 + Math.random() * 0.2;
         this.loadImages(this.IMAGES_WALKING);
         this.animate();
     }
+
     animate(){
+        this.moveLeft();
         setInterval(() => {
             let i = this.currentImage % this.IMAGES_WALKING.length;
             let path = this.IMAGES_WALKING[i];
             this.img = this.imageCache[path];
             this.currentImage++;
-            this.x -= 1;
         },200);
     }
 }
